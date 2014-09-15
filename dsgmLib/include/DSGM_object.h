2756004746aed815bc903568d635579a4e3a25a6
CTurt <cturt@users.noreply.github.com>
2014-09-15 20:02:57 +0100

    Cast object instance

--- dsgmLib/include/DSGM_object.h
+++ dsgmLib/include/DSGM_object.h
@@ -85,7 +85,7 @@ void (DSGM_AddCollisionEvent)(DSGM_Object *object, DSGM_Object *collider, DSGM_C
 #define DSGM_StylusOverObjectInstance(me) DSGM_StylusOverObjectInstanceFull(&DSGM_Rooms[DSGM_currentRoom], (DSGM_ObjectInstance *)me)
 inline bool DSGM_StylusOverObjectInstanceFull(DSGM_Room *room, DSGM_ObjectInstance *me);
 
-#define DSGM_ObjectInstanceOnScreen(me) DSGM_ObjectInstanceOnScreenFull(&DSGM_Rooms[DSGM_currentRoom], me)
+#define DSGM_ObjectInstanceOnScreen(me) DSGM_ObjectInstanceOnScreenFull(&DSGM_Rooms[DSGM_currentRoom], (DSGM_ObjectInstance *)me)
 inline bool DSGM_ObjectInstanceOnScreenFull(DSGM_Room *room, DSGM_ObjectInstance *me);
 
 #define DSGM_ObjectInstanceCollision(me, collider) DSGM_ObjectInstanceCollision((DSGM_ObjectInstance *)me, (DSGM_ObjectInstance *)collider)
