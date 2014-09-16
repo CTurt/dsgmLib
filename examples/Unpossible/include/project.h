4eb501690fdbc0041c28d686a347a2adc89d6b13
CTurt <cturt@users.noreply.github.com>
2014-09-16 23:26:13 +0100

    Unpossible example

---
+++ examples/Unpossible/include/project.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#define DSGM_SOUND_STREAM_COUNT 0
+#define DSGM_SOUND_EFFECT_COUNT 0
+#define DSGM_SOUND_COUNT (DSGM_SOUND_STREAM_COUNT + DSGM_SOUND_EFFECT_COUNT)
+#define DSGM_BACKGROUND_COUNT 1
+#define DSGM_PALETTE_COUNT 1
+#define DSGM_SPRITE_COUNT 2
+#define DSGM_OBJECT_COUNT 2
+#define DSGM_ROOM_COUNT 1
+
+// Include backgrounds, palettes and sprites to be loaded from RAM
+
+// No sounds, no enum
+//typedef enum {
+//} DSGM_SoundNames;
+
+typedef enum {
+	background,
+} DSGM_BackgroundNames;
+
+typedef enum {
+	DSGMPal0,
+} DSGM_PaletteNames;
+
+typedef enum {
+	blockSprite,
+	spikeSprite,
+} DSGM_SpriteNames;
+
+typedef enum {
+	player,
+	spike,
+} DSGM_ObjectNames;
+
+typedef struct {
+	DSGM_ObjectInstanceBase;
+	struct {
+		int vy;
+	} *variables;
+} playerObjectInstance;
+
+typedef struct {
+	DSGM_ObjectInstanceBase;
+	struct {
+	} *variables;
+} spikeObjectInstance;
+
+typedef enum {
+	Room_1,
+} DSGM_RoomNames;
+
+extern DSGM_Sound DSGM_Sounds[DSGM_SOUND_COUNT];
+extern DSGM_Background DSGM_Backgrounds[DSGM_BACKGROUND_COUNT];
+extern DSGM_Palette DSGM_Palettes[DSGM_PALETTE_COUNT];
+extern DSGM_Sprite DSGM_Sprites[DSGM_SPRITE_COUNT];
+extern DSGM_Object DSGM_Objects[DSGM_OBJECT_COUNT];
+extern DSGM_Room DSGM_Rooms[DSGM_ROOM_COUNT];
+
+extern int DSGM_currentRoom;
+
+void DSGM_SetupRooms(int room);
+
+void player_create(playerObjectInstance *me);
+void player_loop(playerObjectInstance *me);
+
+void spike_loop(spikeObjectInstance *me);
+
+void player_collide_spike(playerObjectInstance *me, spikeObjectInstance *collider);
---
+++ examples/Unpossible/source/project.c
@@ -0,0 +1,252 @@
+#include "DSGM.h"
+
+#include "DSGM_projectHelper.h"
+
+// User variables / declarations
+
+// Resources
+DSGM_Sound DSGM_Sounds[DSGM_SOUND_COUNT] = {
+};
+
+DSGM_Background DSGM_Backgrounds[DSGM_BACKGROUND_COUNT] = {
+	DSGM_FORM_NITRO_BACKGROUND(background, BgSize_T_256x256, BgType_Text8bpp),
+};
+
+DSGM_Palette DSGM_Palettes[DSGM_PALETTE_COUNT] = {
+	DSGM_FORM_NITRO_PALETTE(DSGMPal0),
+};
+
+DSGM_Sprite DSGM_Sprites[DSGM_SPRITE_COUNT] = {
+	DSGM_FORM_NITRO_SPRITE(block, DSGMPal0, SpriteSize_32x32, 1),
+	DSGM_FORM_NITRO_SPRITE(spike, DSGMPal0, SpriteSize_16x16, 1),
+};
+
+DSGM_Object DSGM_Objects[DSGM_OBJECT_COUNT] = {
+	// player
+	{
+		&DSGM_Sprites[blockSprite],
+		(DSGM_Event)player_create,
+		(DSGM_Event)player_loop,
+		DSGM_NO_EVENT,
+		DSGM_NO_EVENT,
+		NULL, 0,
+		
+		sizeof(*((playerObjectInstance *)0)->variables)
+	},
+	
+	// spike
+	{
+		&DSGM_Sprites[spikeSprite],
+		DSGM_NO_EVENT,
+		(DSGM_Event)spike_loop,
+		DSGM_NO_EVENT,
+		DSGM_NO_EVENT,
+		NULL, 0,
+		
+		sizeof(*((spikeObjectInstance *)0)->variables)
+	},
+};
+
+DSGM_Room DSGM_Rooms[DSGM_ROOM_COUNT] = {
+	// Room_1
+	{
+		// Backgrounds
+		{
+			// Bottom screen
+			{
+				// Layer 0
+				{
+					DSGM_NO_BACKGROUND,		// Background
+					DSGM_BOTTOM,				// Screen
+					0,							// Layer
+					false,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 1
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_BOTTOM,				// Screen
+					1,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 2
+				{
+					&DSGM_Backgrounds[background],// Background
+					DSGM_BOTTOM,				// Screen
+					2,							// Layer
+					true,						// Attached to view system
+					7,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 3
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_BOTTOM,				// Screen
+					3,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+			},
+			
+			// Top screen
+			{
+				// Layer 0
+				{
+					DSGM_TEXT_BACKGROUND,		// Background
+					DSGM_TOP,					// Screen
+					0,							// Layer
+					false,						// Attached to view system
+					7,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 1
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_TOP,					// Screen
+					1,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 2
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_TOP,					// Screen
+					2,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 3
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_TOP,					// Screen
+					3,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+			}
+		},
+		
+		// Initial views
+		{
+			// Bottom screen
+			{
+				0, 0
+			},
+			
+			// Top screen
+			{
+				0, 0
+			}
+		},
+		
+		// Views
+		{
+			// Bottom screen
+			{
+				0, 0
+			},
+			
+			// Top screen
+			{
+				0, 0
+			}
+		},
+		
+		// Object groups are dynamic, so must be set up at run time, see DSGM_SetupRooms.
+		{
+			NULL,
+			NULL
+		},
+		{
+			0,
+			0
+		}
+	},
+};
+
+int DSGM_currentRoom = Room_1;
+
+void DSGM_SetupRooms(int room) {
+	if(room != DSGM_ALL_ROOMS) {
+		switch(room) {
+			case Room_1: goto Room_1; break;
+		}
+	}
+	
+	Room_1:
+	DSGM_Debug("Room_1 reset\n");
+	DSGM_LeaveRoom(&DSGM_Rooms[Room_1]);
+	
+	DSGM_SetupViews(&DSGM_Rooms[Room_1]);
+	
+	DSGM_SetupObjectGroups(&DSGM_Rooms[Room_1], DSGM_TOP, 0);
+	
+	DSGM_SetupObjectGroups(&DSGM_Rooms[Room_1], DSGM_BOTTOM, 2);
+	
+	DSGM_SetupObjectInstances(&DSGM_Rooms[Room_1].objectGroups[DSGM_BOTTOM][0], &DSGM_Objects[player], DSGM_BOTTOM, 1,
+		32, 129
+	);
+	
+	DSGM_SetupObjectInstances(&DSGM_Rooms[Room_1].objectGroups[DSGM_BOTTOM][1], &DSGM_Objects[spike], DSGM_BOTTOM, 1,
+		255, 145
+	);
+	
+	DSGM_AddCollisionEvent(&DSGM_Objects[player], &DSGM_Objects[spike], player_collide_spike);
+	
+	if(room != DSGM_ALL_ROOMS) return;
+}
+
+void player_create(playerObjectInstance *me) {
+	DSGM_SetBackgroundColour(DSGM_TOP, DSGM_Colour(31, 31, 31));
+	DSGM_SetBackgroundColour(DSGM_BOTTOM, DSGM_Colour(31, 31, 31));
+	
+	DSGM_InitObjectInstanceRotScale(me);
+	me->by = me->y << me->bitshift;
+}
+
+void player_loop(playerObjectInstance *me) {
+	me->variables->vy += 72;
+	me->by += me->variables->vy;
+	(*me->angle) -= 540;
+	
+	if(me->by >= 129 << me->bitshift) {
+		me->by = 129 << me->bitshift;
+		me->variables->vy = 0;
+		(*me->angle) = 0;
+		if(DSGM_held.Stylus || DSGM_held.A) me->variables->vy = -1260;
+	}
+	
+	me->y = me->by >> me->bitshift;
+	
+	DSGM_view[me->screen].x += 2;
+	me->x = DSGM_view[me->screen].x + 32;
+}
+
+void spike_loop(spikeObjectInstance *me) {
+	if(me->x - DSGM_view[me->screen].x <= -16) me->x = DSGM_view[me->screen].x + 256;
+}
+
+void player_collide_spike(playerObjectInstance *me, spikeObjectInstance *collider) {
+	DSGM_SwitchRoom(DSGM_currentRoom, true);
+}
