d057d2c3a2d0eea3b93f2f8d232c388d4db94458
CTurt <cturt@users.noreply.github.com>
2014-09-16 20:56:27 +0100

    removed junk

--- examples/TopDownShooter/source/project.c
+++ examples/TopDownShooter/source/project.c
@@ -210,109 +210,6 @@ void DSGM_SetupRooms(int room) {
 	if(room != DSGM_ALL_ROOMS) return;
 }
 
-
-/*static inline u64 DSGM_Distance(s32 x1, s32 y1, s32 x2, s32 y2) {
-   s64 h = x1 - x2;
-   s64 v = y1 - y2;
-   return(h*h + v*v);
-}
-
-static u16 DSGM_AdjustAngle(u16 angle, s16 anglerot, s32 x1, s32 y1, s32 x2, s32 y2) {
-	u64 distances[3];
-	x1 = x1 << 8;
-	y1 = y1 << 8;
-	x2 = x2 << 8;
-	y2 = y2 << 8;
-	u16 tempangle = (angle - anglerot) & 511;
-	
-	distances[0] = DSGM_Distance(x1 + cosLerp(tempangle), y1 - sinLerp(tempangle), x2, y2);
-	tempangle += anglerot;
-	tempangle &= 511;
-	distances[1] = DSGM_Distance(x1 + cosLerp(tempangle), y1 - sinLerp(tempangle), x2, y2);
-	tempangle += anglerot;
-	tempangle &= 511;
-	distances[2] = DSGM_Distance(x1 + cosLerp(tempangle), y1 - sinLerp(tempangle), x2, y2);
-	
-	if(distances[0] < distances[1])  angle -= anglerot;
-	else if(distances[2] < distances[1])  angle += anglerot;
-	
-	return (angle & 511);
-}
-
-inline u16 DSGM_GetAngle(s32 x1, s32 y1, s32 x2, s32 y2) {
-	u16 angle = 0;
-	u16 anglerot = 180;
-	
-	while(anglerot > 5) {
-		angle = DSGM_AdjustAngle(angle, anglerot, x1, y1, x2, y2);
-		anglerot = (anglerot - ((3 * anglerot) >> 3));
-	}
-	
-	anglerot = 4;
-	angle = DSGM_AdjustAngle(angle, anglerot, x1, y1, x2, y2);
-	anglerot = 2;
-	angle = DSGM_AdjustAngle(angle, anglerot, x1, y1, x2, y2);
-	anglerot = 1;
-	angle = DSGM_AdjustAngle(angle, anglerot, x1, y1, x2, y2);
-	
-	return degreesToAngle(angle);
-}*/
-
-
-static inline u64 PA_Distance(s32 x1, s32 y1, s32 x2, s32 y2) {
-   s64 h = x1 - x2;
-   s64 v = y1 - y2;
-   return(h*h + v*v);
-}
-
-u16 PA_AdjustAngle(u16 angle, s16 anglerot, s32 startx, s32 starty, s32 targetx, s32 targety) {
-	u64 distances[3];
-	startx = startx << 8; // Fixed point...
-	starty = starty << 8; // Fixed point...
-	targetx = targetx << 8; // Fixed point...
-	targety = targety << 8; // Fixed point...
-	u16 tempangle = (angle - anglerot) & 511;
-	// Calcul des distances en fonction des angles
-	//distances[0] = PA_Distance(startx + PA_Cos(tempangle), starty - PA_Sin(tempangle), targetx, targety);
-	distances[0] = PA_Distance(startx + cosLerp(degreesToAngle(tempangle)), starty - sinLerp(degreesToAngle(tempangle)), targetx, targety);
-	tempangle += anglerot;
-	tempangle &= 511;
-	//distances[1] = PA_Distance(startx + PA_Cos(tempangle), starty - PA_Sin(tempangle), targetx, targety);
-	distances[1] = PA_Distance(startx + cosLerp(degreesToAngle(tempangle)), starty - sinLerp(degreesToAngle(tempangle)), targetx, targety);
-	tempangle += anglerot;
-	tempangle &= 511;
-	//distances[2] = PA_Distance(startx + PA_Cos(tempangle), starty - PA_Sin(tempangle), targetx, targety);
-	distances[2] = PA_Distance(startx + cosLerp(degreesToAngle(tempangle)), starty - sinLerp(degreesToAngle(tempangle)), targetx, targety);
-
-	// On regarde si l'angle est optimal. Si ce n'est pas le cas,
-	// on fait tourner toujours dans le meme sens...
-	if (distances[0] < distances[1])  angle -= anglerot;
-	else if (distances[2] < distances[1])  angle += anglerot;
-
-	return angle&511;
-}
-
-static inline u16 DSGM_GetAngle(s32 startx, s32 starty, s32 targetx, s32 targety) {
-	u16 angle = 0;
-	u16 anglerot = 180;
-
-	while(anglerot > 5) {
-		angle = PA_AdjustAngle(angle, anglerot, startx, starty, targetx, targety);
-		anglerot = (anglerot - ((3 * anglerot) >> 3)); // On diminue petit ? petit la rotation...
-	}
-
-	// Ajustement encore plus précis...
-	anglerot = 4;
-	angle = PA_AdjustAngle(angle, anglerot, startx, starty, targetx, targety);
-	anglerot = 2;
-	angle = PA_AdjustAngle(angle, anglerot, startx, starty, targetx, targety);
-	anglerot = 1;
-	angle = PA_AdjustAngle(angle, anglerot, startx, starty, targetx, targety);
-
-	return angle;
-}
-
-
 void player_create(playerObjectInstance *me) {
 	DSGM_DrawText(DSGM_TOP, 1, 1, "D-Pad to move, A to shoot");
 	DSGM_InitObjectInstanceRotScale(me);
@@ -320,11 +217,6 @@ void player_create(playerObjectInstance *me) {
 }
 
 void player_loop(playerObjectInstance *me) {
-	if(DSGM_held.Stylus) {
-		DSGM_DrawText(DSGM_TOP, 5, 5, "Angle      ", DSGM_GetAngle(DSGM_stylus.px, DSGM_stylus.py, me->x, me->y));
-		DSGM_DrawText(DSGM_TOP, 5, 5, "Angle %d", DSGM_GetAngle(DSGM_stylus.px, DSGM_stylus.py, me->x, me->y));
-		(*me->angle) = DSGM_GetAngle(DSGM_stylus.px, DSGM_stylus.py, me->x, me->y);
-	}
 	if(DSGM_held.Left) {
 		(*me->angle) += degreesToAngle(4);
 	}
