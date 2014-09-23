08e5cbcdb3fddc6b4b505441fabb863f799919d6
CTurt <cturt@users.noreply.github.com>
2014-09-23 17:00:54 +0100

    Set text color of custom fonts

--- dsgmLib/source/DSGM_text.c
+++ dsgmLib/source/DSGM_text.c
@@ -38,9 +38,13 @@ inline void DSGM_SetTextLayer(u8 screen, u8 layerNumber) {
 	DSGM_textLayer[screen] = layerNumber;
 }
 
-// Todo: work for custom fonts
 inline void DSGM_SetTextColor(u8 screen, u16 color) {
-	(screen == DSGM_TOP ? BG_PALETTE : BG_PALETTE_SUB)[15 + (15 * 16)] = color;
+	if(DSGM_text[screen][DSGM_textLayer[screen]].font.gfx == consoleGetDefault()->font.gfx) {
+		(screen == DSGM_TOP ? BG_PALETTE : BG_PALETTE_SUB)[15 + (15 * 16)] = color;
+	}
+	else {
+		(screen == DSGM_TOP ? BG_PALETTE : BG_PALETTE_SUB)[1] = color;
+	}
 }
 
 void DSGM_ClearText(u8 screen) {
