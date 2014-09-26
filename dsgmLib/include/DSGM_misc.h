a4349bda47affa2b6f37c708a09c8e3e246bfcae
CTurt <cturt@users.noreply.github.com>
2014-09-26 16:50:45 +0100

    DSGM_SetBrightness

--- dsgmLib/include/DSGM_misc.h
+++ dsgmLib/include/DSGM_misc.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define DSGM_SetBrightness(screen, brightness) setBrightness(screen, brightness)
+
 inline u16 DSGM_GetAngle(s32 startx, s32 starty, s32 targetx, s32 targety);
 
 inline int DSGM_Random(int min, int max);
