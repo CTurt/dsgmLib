c6849bc540cbae4f6b43f12dcdb58ed32ecaad8e
CTurt <cturt@users.noreply.github.com>
2014-09-30 17:35:59 +0100

    NiFi example

---
+++ examples/NiFi/include/project.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#define DSGM_SOUND_STREAM_COUNT 0
+#define DSGM_SOUND_EFFECT_COUNT 0
+#define DSGM_SOUND_COUNT (DSGM_SOUND_STREAM_COUNT + DSGM_SOUND_EFFECT_COUNT)
+#define DSGM_BACKGROUND_COUNT 0
+#define DSGM_PALETTE_COUNT 1
+#define DSGM_SPRITE_COUNT 1
+#define DSGM_OBJECT_COUNT 1
+#define DSGM_ROOM_COUNT 1
+
+// Include backgrounds, palettes and sprites to be loaded from RAM
+#include "DSGMPal0_Pal_bin.h"
+#include "ball_Sprite_bin.h"
+
+// No sounds, no enum
+//typedef enum {
+//} DSGM_SoundNames;
+
+// No backgrounds, no enum
+//typedef enum {
+//} DSGM_BackgroundNames;
+
+// No palettes, no enum
+typedef enum {
+  DSGMPal0,
+} DSGM_PaletteNames;
+
+// No sprites, no enum
+typedef enum {
+  ball,
+} DSGM_SpriteNames;
+
+typedef enum {
+	hello,
+} DSGM_ObjectNames;
+
+typedef struct {
+	DSGM_ObjectInstanceBase;
+	struct {
+	} *variables;
+} helloObjectInstance;
+
+typedef enum {
+	Room_1,
+} DSGM_RoomNames;
+
+extern DSGM_Sound DSGM_Sounds[DSGM_SOUND_COUNT];
+extern DSGM_Background DSGM_Backgrounds[DSGM_BACKGROUND_COUNT];
+extern DSGM_Palette DSGM_Palettes[DSGM_PALETTE_COUNT];
+extern DSGM_Sprite DSGM_Sprites[DSGM_SPRITE_COUNT];
+extern DSGM_Object DSGM_Objects[DSGM_OBJECT_COUNT];
+extern DSGM_Room DSGM_Rooms[DSGM_ROOM_COUNT];
+
+extern int DSGM_currentRoom;
+
+void DSGM_SetupRooms(int room);
+
+void hello_create(helloObjectInstance *me);
+void hello_loop(helloObjectInstance *me);
---
+++ examples/NiFi/source/project.c
@@ -0,0 +1,219 @@
+#include "DSGM.h"
+
+#include "DSGM_projectHelper.h"
+
+// User variables / declarations
+
+// Resources
+DSGM_Sound DSGM_Sounds[DSGM_SOUND_COUNT] = {
+};
+
+DSGM_Background DSGM_Backgrounds[DSGM_BACKGROUND_COUNT] = {
+};
+
+DSGM_Palette DSGM_Palettes[DSGM_PALETTE_COUNT] = {
+  DSGM_FORM_RAM_PALETTE(DSGMPal0),
+};
+
+DSGM_Sprite DSGM_Sprites[DSGM_SPRITE_COUNT] = {
+  DSGM_FORM_RAM_SPRITE(ball, DSGMPal0, SpriteSize_32x32, 1),
+};
+
+DSGM_Object DSGM_Objects[DSGM_OBJECT_COUNT] = {
+	// hello
+	{
+		&DSGM_Sprites[ball],
+		(DSGM_Event)hello_create,
+		(DSGM_Event)hello_loop,
+		DSGM_NO_EVENT,
+		DSGM_NO_EVENT,
+		NULL, 0,
+		
+		sizeof(*((helloObjectInstance *)0)->variables)
+	},
+};
+
+DSGM_Room DSGM_Rooms[DSGM_ROOM_COUNT] = {
+	// Room_1
+	{
+		// Backgrounds
+		{
+			// Bottom screen
+			{
+				// Layer 0
+				{
+					DSGM_DEFAULT_FONT,			// Background
+					DSGM_BOTTOM,				// Screen
+					0,							// Layer
+					false,						// Attached to view system
+					7,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 1
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_BOTTOM,				// Screen
+					1,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 2
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_BOTTOM,				// Screen
+					2,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 3
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_BOTTOM,				// Screen
+					3,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+			},
+			
+			// Top screen
+			{
+				// Layer 0
+				{
+					DSGM_DEFAULT_FONT,			// Background
+					DSGM_TOP,					// Screen
+					0,							// Layer
+					false,						// Attached to view system
+					7,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 1
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_TOP,					// Screen
+					1,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 2
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_TOP,					// Screen
+					2,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+				
+				// Layer 3
+				{
+					DSGM_NO_BACKGROUND,			// Background
+					DSGM_TOP,					// Screen
+					3,							// Layer
+					true,						// Attached to view system
+					0,							// Map base
+					0,							// Tile base
+					0, 0, 0
+				},
+			}
+		},
+		
+		// Initial views
+		{
+			// Bottom screen
+			{
+				0, 0
+			},
+			
+			// Top screen
+			{
+				0, 0
+			}
+		},
+		
+		// Views
+		{
+			// Bottom screen
+			{
+				0, 0
+			},
+			
+			// Top screen
+			{
+				0, 0
+			}
+		},
+		
+    NULL,
+    
+		// Object groups are dynamic, so must be set up at run time, see DSGM_SetupRooms.
+		{
+			NULL,
+			NULL
+		},
+		{
+			0,
+			0
+		}
+	},
+};
+
+int DSGM_currentRoom = Room_1;
+
+void DSGM_SetupRooms(int room) {
+	if(room != DSGM_ALL_ROOMS) {
+		switch(room) {
+			case Room_1: goto Room_1; break;
+		}
+	}
+	
+	Room_1:
+	DSGM_Debug("Room_1 reset\n");
+	DSGM_LeaveRoom(&DSGM_Rooms[Room_1]);
+	
+	DSGM_SetupViews(&DSGM_Rooms[Room_1]);
+	
+	DSGM_SetupObjectGroups(&DSGM_Rooms[Room_1], DSGM_TOP, 0);
+	
+	DSGM_SetupObjectGroups(&DSGM_Rooms[Room_1], DSGM_BOTTOM, 1);
+	
+	DSGM_SetupObjectInstances(&DSGM_Rooms[Room_1].objectGroups[DSGM_BOTTOM][0], &DSGM_Objects[hello], DSGM_BOTTOM, 1,
+		0, 0
+	);
+	
+	if(room != DSGM_ALL_ROOMS) return;
+}
+
+void hello_create(helloObjectInstance *me) {
+  DSGM_InitNiFi();
+}
+
+void hello_loop(helloObjectInstance *me) {
+  unsigned short buffer[8];
+  
+  if(DSGM_held.Stylus) {
+    me->x = DSGM_stylus.x - 16;
+    me->y = DSGM_stylus.y - 16;
+    memcpy(buffer, &me->x, 8);
+    DSGM_SendWirelessData(buffer, 8);
+  }
+  
+  if(DSGM_newWirelessData) {
+    memcpy(&me->x, DSGM_wirelessData, 8);
+  }
+}
