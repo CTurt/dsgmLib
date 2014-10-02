c06cc5e51002af19d46fccc771a43e7dc9264e6a
CTurt <cturt@users.noreply.github.com>
2014-10-02 17:32:03 +0100

    Better stylus handling

--- dsgmLib/source/DSGM_input.c
+++ dsgmLib/source/DSGM_input.c
@@ -9,13 +9,6 @@ void DSGM_UpdateInput(void) {
 	int newpress = keysDown();
 	int held = keysHeld();
 	int release = keysUp();
-	
-  touchPosition tempStylus;
-	touchRead(&tempStylus);
-  DSGM_stylus.dx = tempStylus.px - DSGM_stylus.x;
-  DSGM_stylus.dy = tempStylus.py - DSGM_stylus.y;
-	DSGM_stylus.x = tempStylus.px;
-	DSGM_stylus.y = tempStylus.py;
   
 	#define MAP_KEY(DSGMKey, libndsKey)\
 	DSGM_newpress.DSGMKey = (newpress & libndsKey);\
@@ -42,4 +35,17 @@ void DSGM_UpdateInput(void) {
   DSGM_newpress.Any = (DSGM_newpress.A || DSGM_newpress.B || DSGM_newpress.Select || DSGM_newpress.Start || DSGM_newpress.Right || DSGM_newpress.Left || DSGM_newpress.Up || DSGM_newpress.Down || DSGM_newpress.R || DSGM_newpress.L || DSGM_newpress.X || DSGM_newpress.Y || DSGM_newpress.Stylus || DSGM_newpress.Lid);
   DSGM_held.Any = (DSGM_held.A || DSGM_held.B || DSGM_held.Select || DSGM_held.Start || DSGM_held.Right || DSGM_held.Left || DSGM_held.Up || DSGM_held.Down || DSGM_held.R || DSGM_held.L || DSGM_held.X || DSGM_held.Y || DSGM_held.Stylus || DSGM_held.Lid);
   DSGM_release.Any = (DSGM_release.A || DSGM_release.B || DSGM_release.Select || DSGM_release.Start || DSGM_release.Right || DSGM_release.Left || DSGM_release.Up || DSGM_release.Down || DSGM_release.R || DSGM_release.L || DSGM_release.X || DSGM_release.Y || DSGM_release.Stylus || DSGM_release.Lid);
+  
+  if(DSGM_held.Stylus) {
+    touchPosition tempStylus;
+    touchRead(&tempStylus);
+    DSGM_stylus.dx = tempStylus.px - DSGM_stylus.x;
+    DSGM_stylus.dy = tempStylus.py - DSGM_stylus.y;
+    DSGM_stylus.x = tempStylus.px;
+    DSGM_stylus.y = tempStylus.py;
+  }
+  else {
+    DSGM_stylus.dx = 0;
+    DSGM_stylus.dy = 0;
+  }
 }
