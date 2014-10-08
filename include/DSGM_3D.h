4b203cd2ff87ff69dcfb24abaf426a491a294229
CTurt <cturt@users.noreply.github.com>
2014-10-08 19:46:00 +0100

    Further OpenGL compatibility

--- include/DSGM_3D.h
+++ include/DSGM_3D.h
@@ -2,4 +2,7 @@
 
 #define DSGM_3D_RENDERER (void *)3
 
+#define glVertex3fv(v) glVertex3f(v[0], v[1], v[2])
+#define glNormal3fv(n) glNormal3f(n[0], n[1], n[2])
+
 void DSGM_DrawCube(float x, float y, float z, float width, float height, float depth);
