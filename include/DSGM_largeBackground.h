56cbd076d5c2b81f8c7ed3cdb3ae4cdae4ef613e
CTurt <cturt@users.noreply.github.com>
2014-10-18 08:59:05 +0100

    Large background copy to screen

--- include/DSGM_largeBackground.h
+++ include/DSGM_largeBackground.h
@@ -11,3 +11,5 @@ typedef enum {
 } DSGM_LARGE_BACKGROUND_SIZES;
 
 inline int DSGM_LargeBackgroundSizeToPixels(DSGM_LARGE_BACKGROUND_SIZES size);
+void DSGM_LargeBackgroundCopyScreen(DSGM_Layer *layer);
+void DSGM_LargeBackgroundUpdate(DSGM_Layer *layer);
--- source/DSGM_largeBackground.c
+++ source/DSGM_largeBackground.c
@@ -3,3 +3,30 @@
 inline int DSGM_LargeBackgroundSizeToPixels(DSGM_LARGE_BACKGROUND_SIZES size) {
 	return 1 << (size + 9);
 }
+
+void DSGM_LargeBackgroundCopyScreen(DSGM_Layer *layer) {
+	int tilesLength = DSGM_LargeBackgroundSizeToPixels(layer->background->size) / 8;
+	
+	int xl, yl;
+	for(xl = 0; xl < tilesLength; xl++) {
+		for(yl = 0; yl < tilesLength; yl++) {
+			int x = xl;
+			int y = yl;
+			
+			/*while(y > 31) {
+				y -= 32;
+				x += 64;
+			}
+			while(x > 31) {
+				x -= 32;
+				y += 32;
+			}*/
+			
+			DSGM_SetTileFull(layer, x, y, layer->largeBackgroundMap[y * tilesLength / 2 + x]);
+		}
+	}
+}
+
+void DSGM_LargeBackgroundUpdate(DSGM_Layer *layer) {
+	
+}
