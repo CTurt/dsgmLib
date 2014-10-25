6d9d6c93993afee6e9a331a77c2fec893c59947f
CTurt <cturt@users.noreply.github.com>
2014-10-25 09:46:11 +0100

    Moved vector code

--- include/DSGM.h
+++ include/DSGM.h
@@ -36,6 +36,7 @@ typedef char DSGM_CharStructLabel[0];
 typedef short DSGM_ShortStructLabel[0];
 typedef int DSGM_IntStructLabel[0];
 
+#include "DSGM_vector.h"
 #include "DSGM_3D.h"
 #include "DSGM_atan2Lerp.h"
 #include "DSGM_misc.h"
