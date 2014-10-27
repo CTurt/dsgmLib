fb1fc304d41e0f9824e2cca8096f2c0e95773d0f
CTurt <cturt@users.noreply.github.com>
2014-10-27 11:03:20 +0000

    Revert "Pass room when loading palette"
    This reverts commit 828dc21d0476699f0ad083afa64d01401667a8ea.

--- source/DSGM_palette.c
+++ source/DSGM_palette.c
@@ -58,8 +58,8 @@ void DSGM_ClearPalettes(DSGM_Palette *palettes, int paletteCount) {
 	}
 }
 
-void DSGM_LoadPaletteFull(DSGM_Room *room, u8 screen, DSGM_Palette *palette) {
-	DSGM_UnlockSpritePalette(room, screen);
+void DSGM_LoadPaletteFull(u8 screen, DSGM_Palette *palette) {
+	DSGM_UnlockSpritePalette(screen);
 	palette->paletteNumber[screen] = DSGM_NextFreePalette(screen);
 	if(DSGM_PaletteIsNitro(palette)) {
 		if(screen == DSGM_TOP) DSGM_ReadFileManual(VRAM_F_EXT_SPR_PALETTE[palette->paletteNumber[screen]], 0, DSGM_AUTO_LENGTH, palette->nitroFilename);
@@ -69,5 +69,5 @@ void DSGM_LoadPaletteFull(DSGM_Room *room, u8 screen, DSGM_Palette *palette) {
 		if(screen == DSGM_TOP) dmaCopy(palette->palette, VRAM_F_EXT_SPR_PALETTE[palette->paletteNumber[screen]], *palette->paletteLength);
 		if(screen == DSGM_BOTTOM) dmaCopy(palette->palette, VRAM_I_EXT_SPR_PALETTE[palette->paletteNumber[screen]], *palette->paletteLength);
 	}
-	DSGM_LockSpritePalette(room, screen);
+	DSGM_LockSpritePalette(screen);
 }
