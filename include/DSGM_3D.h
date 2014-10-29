90f2fb8ba9d70f25c71e9dc3347819385bfc8177
CTurt <cturt@users.noreply.github.com>
2014-10-29 14:35:32 +0000

    DSGM camera

--- include/DSGM_3D.h
+++ include/DSGM_3D.h
@@ -9,6 +9,14 @@
 #define glVertex3fv(v) glVertex3f(v[0], v[1], v[2])
 #define glNormal3fv(n) glNormal3f(n[0], n[1], n[2])
 
+typedef struct {
+	vect3D position;
+	vect3D lookAt;
+	vect3D up;
+} DSGM_Camera;
+
+inline void DSGM_UseCamera(DSGM_Camera *camera);
+
 inline u64 DSGM_Distance3D(s32 x1, s32 y1, s32 z1, s32 x2, s32 y2, s32 z2);
 
 void DSGM_GetPositionMatrix(struct m4x4 *m);
--- source/DSGM_3D.c
+++ source/DSGM_3D.c
@@ -1,5 +1,10 @@
 #include "DSGM.h"
 
+inline void DSGM_UseCamera(DSGM_Camera *camera) {
+	//glLoadIdentity();
+	gluLookAtf32(camera->position.x, camera->position.y, camera->position.z, camera->lookAt.x, camera->lookAt.y, camera->lookAt.z, camera->up.x, camera->up.y, camera->up.z);
+}
+
 inline u64 DSGM_Distance3D(s32 x1, s32 y1, s32 z1, s32 x2, s32 y2, s32 z2) {
 	s64 h = x1 - x2;
 	s64 v = y1 - y2;
