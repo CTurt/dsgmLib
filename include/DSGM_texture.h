de21f941de90714d155df75727fde311c5c46649
CTurt <cturt@users.noreply.github.com>
2014-11-03 16:44:30 +0000

    Basic 16bit texture loading abstractions

--- include/DSGM_texture.h
+++ include/DSGM_texture.h
@@ -1,2 +1,13 @@
 #pragma once
 
+#define DSGM_FORM_RAM_TEXTURE(type, width, height, data) { type, width, height, data, -1 }
+
+typedef struct {
+	GL_TEXTURE_TYPE_ENUM type;
+	enum GL_TEXTURE_SIZE_ENUM width;
+	enum GL_TEXTURE_SIZE_ENUM height;
+	const unsigned char *data;
+	int id;
+} DSGM_Texture;
+
+void DSGM_LoadTexture(DSGM_Texture *texture);
--- source/DSGM_texture.c
+++ source/DSGM_texture.c
@@ -1,2 +1,12 @@
 #include "DSGM.h"
 
+void DSGM_LoadTexture(DSGM_Texture *texture) {
+	// Doesn't support paletted textures
+	if(!texture || (texture->type != GL_RGB && texture->type != GL_RGBA)) {
+		return;
+	}
+	
+	glGenTextures(1, &texture->id);
+	glBindTexture(0, texture->id);
+	glTexImage2D(0, 0, texture->type, texture->width, texture->height, 0, TEXGEN_TEXCOORD, texture->data);
+}
