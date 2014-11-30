15dee15e97194ff5e211bc56052b4960ef3bba53
CTurt <cturt@users.noreply.github.com>
2014-11-30 11:44:16 +0000

    Start saving, start loading

--- include/DSGM_saving.h
+++ include/DSGM_saving.h
@@ -2,3 +2,6 @@
 
 extern FILE *DSGM_save;
 extern char DSGM_saveName[256];
+
+inline bool DSGM_StartSaving(void);
+inline void DSGM_FinishSaving(void);
--- source/DSGM_saving.c
+++ source/DSGM_saving.c
@@ -2,3 +2,13 @@
 
 FILE *DSGM_save;
 char DSGM_saveName[256];
+
+// Saving applies to both saving and loading
+inline bool DSGM_StartSaving(void) {
+	DSGM_save = fopen(DSGM_saveName, "r+b");
+	return DSGM_save ? true : false;
+}
+
+inline void DSGM_FinishSaving(void) {
+	fclose(DSGM_save);
+}
