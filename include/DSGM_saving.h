d4dacec3eeac2117c5db235aef7ed1ae6242feae
CTurt <cturt@users.noreply.github.com>
2015-02-14 16:21:24 +0000

    Room saving

--- include/DSGM_saving.h
+++ include/DSGM_saving.h
@@ -5,3 +5,21 @@ extern char DSGM_saveName[256];
 
 inline bool DSGM_StartSaving(void);
 inline void DSGM_FinishSaving(void);
+
+#define DSGM_WriteRoom() DSGM_WriteRoomFull(&DSGM_Rooms[DSGM_currentRoom])
+void DSGM_WriteRoomFull(DSGM_Room *room);
+
+/*#define DSGM_ReadRoom() do {\
+	DSGM_ReadRoomFull(&DSGM_Rooms[DSGM_currentRoom]);\
+	DSGM_ResetAll();\
+	DSGM_LoadRoom(&DSGM_Rooms[DSGM_currentRoom]);\
+	return;\
+} while(0)*/
+
+/*#define DSGM_ReadRoom() do {\
+	DSGM_ReadRoomFull(&DSGM_Rooms[DSGM_currentRoom]);\
+	DSGM_SwitchRoom(DSGM_currentRoom, false);\
+} while(0)*/
+
+#define DSGM_ReadRoom() DSGM_ReadRoomFull(&DSGM_Rooms[DSGM_currentRoom])
+void DSGM_ReadRoomFull(DSGM_Room *room);
