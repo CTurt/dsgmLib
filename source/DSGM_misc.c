a1d27222566a19c814152140cd7eb1a9d056f021
CTurt <cturt@users.noreply.github.com>
2015-04-19 10:27:54 +0100

    Fix random generator

--- source/DSGM_misc.c
+++ source/DSGM_misc.c
@@ -104,7 +104,7 @@ inline u16 DSGM_GetAngle(s32 startx, s32 starty, s32 targetx, s32 targety) {
 }
 
 inline int DSGM_Random(int min, int max) {
-	return (rand() % (max - min)) + min;
+	return (rand() % (max - min + 1)) + min;
 }
 
 void DSGM_Delay(unsigned int time) {
